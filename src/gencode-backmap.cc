2abe56dff4e13ec40ef1a2b4d164023efd55677d
Mark Diekhans <markd@soe.ucsc.edu>
2015-07-21 02:29:08 -0700

    read chains

--- src/gencode-backmap.cc
+++ src/gencode-backmap.cc
@@ -5,19 +5,32 @@
 #include "gxf.hh"
 #include "typeOps.hh"
 #include <fstream>
+#include "transMapper.hh"
 #include "geneMapper.hh"
 
+/* map to different assembly */
+static void gencodeBackmap(const string& inGxfFile,
+                           GxfFormat gxfFormat,
+                           const string& mappingChains,
+                           const string& outGxfFile) {
+    TransMapper transMapper(mappingChains, false);
+    GxfParser gxfParser(inGxfFile, gxfFormat);
+    ofstream outGxf(outGxfFile);
+    GeneMapper geneMapper(&transMapper);
+    geneMapper.mapGxf(&gxfParser, outGxf);
+}
+
+/* Entry point.  Parse arguments. */
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        errAbort(toCharStr("wrong # args: %s inGxf outGxf"), argv[0]);
+    if (argc != 4) {
+        errAbort(toCharStr("wrong # args: %s inGxf mappingChains outGxf"), argv[0]);
     }
     string inGxfFile = argv[1];
-    string outGxfFile = argv[2];
-    
-    GxfParser gxfParser(inGxfFile,
-                        stringEndsWith(inGxfFile, ".gff3") ? GFF3_FORMAT : GTF_FORMAT);
-    ofstream outGxf(outGxfFile);
-    GeneMapper geneMapper(NULL);
-    geneMapper.mapGxf(&gxfParser, outGxf);
+    GxfFormat gxfFormat = stringEndsWith(inGxfFile, ".gff3") ? GFF3_FORMAT : GTF_FORMAT;
+    string mappingChains = argv[2];
+    string outGxfFile = argv[3];
+
+    gencodeBackmap(inGxfFile, gxfFormat, mappingChains, outGxfFile);
+
     return 0;
 }
