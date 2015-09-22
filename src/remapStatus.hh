6e4aced9a43e73fec38ed7a4fe1708193a383204
Mark Diekhans <markd@soe.ucsc.edu>
2015-09-22 15:27:45 -0700

    don't distinguish better partial contiguois and partial fragment

--- src/remapStatus.hh
+++ src/remapStatus.hh
@@ -13,8 +13,7 @@ typedef enum {
     REMAP_STATUS_NONE,               // not set
     REMAP_STATUS_FULL_CONTIG,        // full remap, contiguous
     REMAP_STATUS_FULL_FRAGMENT,      // full remap, fragmented
-    REMAP_STATUS_PARTIAL_CONTIG,     // partial remap, contiguous
-    REMAP_STATUS_PARTIAL_FRAGMENT,   // partial remap, fragmented
+    REMAP_STATUS_PARTIAL,            // partial remap
     REMAP_STATUS_DELETED,            // completely deleted
     REMAP_STATUS_NO_SEQ_MAP,         // no mapping alignments for sequence
     REMAP_STATUS_GENE_CONFLICT,      // transcripts disagree on sequence/strand
@@ -26,7 +25,7 @@ typedef enum {
 RemapStatus remapStatusChildUpdate(RemapStatus parentStatus,
                                    RemapStatus childStatus);
 
-/* convert a remap status to a string  */
+/* Convert a remap status to a string  */
 const string& remapStatusToStr(RemapStatus remapStatus);
    
 
